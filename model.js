3d8cf749bde18f4328e0d25d81d0021ef469a4f7
Runeony1 <tzhu@exeter.edu>
2016-01-24 05:57:02 -0500

    fix error

--- model.js
+++ model.js
@@ -91,7 +91,7 @@ $(function(){
       ctx.stroke();
 
       sC++;
-      output.html("Equation: "+equation.toString()+" FPS: "+FPS+"<br/> Domain: " + dLower + ", " + dUpper+" Position: "+node.x+", "+node.y+" );
+      output.html("Equation: "+equation.toString()+" FPS: "+FPS+"<br/> Domain: " + dLower + ", " + dUpper+" Position: "+node.x+", "+node.y);
 
     }, 1000/FPS)//EOInterval
 
